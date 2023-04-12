78b09c2120ec5e87c97b6b2cea37562dbf7415da
lauren36 <111887636+lauren36@users.noreply.github.com>
2023-04-12 09:37:09 -0500

    Initial commit

---
+++ Classes_Omalley_Lauren/Greeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Omalley_Lauren
+{
+    class Greeting
+    {
+
+        public void Welcome()
+
+        {
+            // Welcomes the user
+            Console.WriteLine("Hello Traveler, welcome!");
+        }
+
+        public void Hello(string name)
+        {
+            // Tells the user thank you for joining us today
+            Console.WriteLine($"{name}, thank you for joining us today!");
+        }
+
+    }
+}
---
+++ Classes_Omalley_Lauren/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Omalley_Lauren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates the greeting object and calls the Welcome method
+            Greeting greeting = new Greeting();
+            greeting.Welcome();
+
+            // Askes the user for their name and has a string for the response
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+
+            // Passes the users name as an argument
+            greeting.Hello(userName);
+        }
+    }
+}
